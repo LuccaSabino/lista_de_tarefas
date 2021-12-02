6b7b1119c3aa13ea75ca69f377d4cae9eea0e808
Lucca Sabino <94568969+LuccaSabino@users.noreply.github.com>
2021-12-01 22:55:49 -0300

    Lista de Tarefas (segunda parte incompleta)

--- itens.js
+++ itens.js
@@ -1,10 +1,11 @@
-
-
-
 function addItem() {
+    var novaTarefa = document.getElementById('nova-tarefa').value
+
     var lista = document.getElementById("lista")
     var item = document.getElementById('item')
     var itens = item.content.cloneNode(true)
+
+    // alert(novaTarefa)
 
     lista.appendChild(itens)
 }
