5c6ef7f9a002d821e538b2efba4d181b5e6ce1a0
Lucca Sabino <94568969+LuccaSabino@users.noreply.github.com>
2021-12-01 22:04:29 -0300

    Lista de Tarefas(em andamento)

---
+++ itens.js
@@ -0,0 +1,10 @@
+
+
+
+function addItem() {
+    var lista = document.getElementById("lista")
+    var item = document.getElementById('item')
+    var itens = item.content.cloneNode(true)
+
+    lista.appendChild(itens)
+}
